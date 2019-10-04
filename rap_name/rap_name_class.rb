4b4bb192da621c15680aab557103c87ce2a88c23
mustafayusuf01 <46446862+mustafayusuf01@users.noreply.github.com>
2019-10-04 12:46:07 +0100

    Add files via upload

---
+++ rap_name/rap_name_class.rb
@@ -0,0 +1,32 @@
+class Rap_name
+  def initialize(name, number = 2, title = "Captain", acronym = "O.A.P")
+    @name = name
+    @number = number
+    @title = title
+    @acronym = acronym
+
+    @adjective = File.readlines("/Users/mustafayusuf/Downloads/RubyExercises/rap_name/adj.adj").sample
+    @adverb = File.readlines("/Users/mustafayusuf/Downloads/RubyExercises/rap_name/adv.adv").sample
+    @noun = File.readlines("/Users/mustafayusuf/Downloads/RubyExercises/rap_name/noun.txt").sample
+    @verb = File.readlines("/Users/mustafayusuf/Downloads/RubyExercises/rap_name/verb.verb").sample
+    @shook = false
+  end
+
+  def shake()
+    @all_names = ["#{@number} #{@noun}", "#{@adjective} #{@name} the #{@noun}",
+    "#{@name} the #{@noun}", "#{@adjective} #{@name}", "#{@noun} #{@name}", "#{@adjective} #{@noun}",
+    "#{@title} #{@name}", "#{@title} #{@adjective}", "#{@title} #{@noun}", "#{@adjective} #{@adjective}",
+    "#{@adjective} #{@acronym}", "#{@noun} #{@noun}"]
+    @shook = true
+    @all_names.sample
+  end
+
+  def all()
+      if @shook == true
+        puts @all_names.each {|a| puts a}
+      else
+        puts "Shake first!"
+      end
+  end
+
+end
